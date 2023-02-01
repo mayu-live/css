4b7e053e48f5bb3cd38dc823523e969b0032e29d
Andreas Alin <andreas.alin@gmail.com>
2023-01-31 21:31:54 -0500

    visitor based on examples I found

--- demo.rb
+++ demo.rb
@@ -12,9 +12,23 @@ puts MayuCSS.minify(__FILE__, <<~CSS)
 }
 CSS
 
+puts "############"
+
 pp JSON.parse(MayuCSS.serialize(__FILE__, <<~CSS))
 .foo {
   background: rgb(50 32 42 / 50%);
+}
+.bar {
+  background: rgb(50 32 42 / 50%);
+}
+CSS
+
+puts "############"
+
+puts MayuCSS.transform(__FILE__, <<~CSS)
+.foo {
+  background: rgb(50 32 42 / 50%);
+  background-image: url("foobar.png");
 }
 .bar {
   background: rgb(50 32 42 / 50%);
--- ext/mayucss/src/lib.rs
+++ ext/mayucss/src/lib.rs
@@ -1,8 +1,44 @@
+use std::collections::HashMap;
 use magnus::{define_module, function, method, prelude::*, wrap, Error};
-use lightningcss::stylesheet::{
-  StyleSheet, ParserOptions, MinifyOptions, PrinterOptions
+use lightningcss::{
+  stylesheet::{StyleSheet, ParserOptions, MinifyOptions, PrinterOptions},
+  visitor::{Visit, VisitTypes, Visitor},
+  visit_types,
+  selector::{Component, Selector},
+  declaration::DeclarationBlock,
 };
 use lightningcss::dependencies::DependencyOptions;
+
+struct MyVisitor;
+
+impl<'i> Visitor<'i> for MyVisitor {
+  const TYPES: VisitTypes = visit_types!(SELECTORS);
+
+  fn visit_selector(&mut self, selector: &mut Selector<'i>) {
+    for c in selector.iter_mut_raw_match_order() {
+
+      match c {
+        Component::Class(c) => {
+          *c = format!("tw-{}", c).into();
+        }
+        _ => {}
+      }
+    }
+  }
+}
+
+fn to_css(stylesheet: StyleSheet) -> String {
+  let res = stylesheet.to_css(
+    PrinterOptions {
+      analyze_dependencies: Some(DependencyOptions::default()),
+      ..PrinterOptions::default()
+    }
+  ).unwrap();
+
+  return res.code;
+}
+
+//////////////
 
 fn serialize(filename: String, source: String) -> String {
   let stylesheet = StyleSheet::parse(
@@ -12,8 +48,23 @@ fn serialize(filename: String, source: String) -> String {
       ..ParserOptions::default()
     },
   ).unwrap();
-
   return serde_json::to_string(&stylesheet).unwrap();
+}
+
+fn transform(filename: String, source: String) -> String {
+  let mut stylesheet = StyleSheet::parse(
+    &source,
+    ParserOptions {
+      filename: filename,
+      ..ParserOptions::default()
+    },
+  ).unwrap();
+  // let mut style_rules = HashMap::new();
+
+  // stylesheet.visit(&mut StyleRuleCollector { });
+  stylesheet.visit(&mut MyVisitor);
+
+  return to_css(stylesheet);
 }
 
 fn minify(filename: String, source: String) -> String {
@@ -29,14 +80,7 @@ fn minify(filename: String, source: String) -> String {
     MinifyOptions::default()
   ).unwrap();
 
-  let res = stylesheet.to_css(
-    PrinterOptions {
-      analyze_dependencies: Some(DependencyOptions::default()),
-      ..PrinterOptions::default()
-    }
-  ).unwrap();
-
-  return res.code;
+  return to_css(stylesheet);
 }
 
 #[magnus::init]
@@ -44,5 +88,6 @@ fn init() -> Result<(), Error> {
     let module = define_module("MayuCSS")?;
     module.define_singleton_method("minify", function!(minify, 2))?;
     module.define_singleton_method("serialize", function!(serialize, 2))?;
+    module.define_singleton_method("transform", function!(transform, 2))?;
     Ok(())
 }
